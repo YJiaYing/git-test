6c5a50f1a7e744f0da1a60a48cda11afb264b5b5
Yang <Yang@atguigu.com>
2022-05-07 15:39:26 +0800

    first commit

---
+++ src/main/java/com/atHome/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atHome.git;
+
+/**
+ * @Author: yjy
+ * @Date: 2022/5/7 15:34
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
